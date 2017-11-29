0805f767b3093d06e018f34af7be90b938f46918
Oliver Kamer <oliver@kamer.ch>
2017-11-29 11:16:54 +0100

    Finished exercise 2a)

--- task 2/vehicle.py
+++ task 2/vehicle.py
@@ -1,48 +1,53 @@
 import sys
 
+
 class Vehicle(object):
-    ######## CODE MISSING HERE
+    vehicle_id = 0
+    vehicles_sold = []
 
     def __init__(self,year,mileage,purchase_price,serial_number):
-        pass
-    ######## CODE MISSING HERE
-        
-    ######## CODE MISSING HERE
+        self._year = year
+        self._mileage = mileage
+        self._purchase_price = purchase_price
+        self._serial_number = serial_number
+        self._vehicle_id = Vehicle.generate_vehicle_id(self)
+        Vehicle.vehicle_id += 1
 
     def __str__(self):
-        pass
-    ######## CODE MISSING HERE
+        return str(self.get_id())
 
     def get_id(self):
-        pass
-    ######## CODE MISSING HERE
+        return self._vehicle_id
 
     @staticmethod
     def generate_vehicle_id(self):
-        pass
-    ######## CODE MISSING HERE
+        return Vehicle.vehicle_id + 100000
 
 class Car(Vehicle):
 
     def __init__(self,year,mileage,purchase_price,serial_number,doors):
-        pass
-    ######## CODE MISSING HERE
+        super().__init__(year, mileage, purchase_price, serial_number)
+        self._doors = doors
+        self._wheels = 4
 
 
 class Lorry(Vehicle):
 
     def __init__(self,year,mileage,purchase_price,serial_number,wheels,doors=2):
-        pass
-    ######## CODE MISSING HERE
+        super().__init__(year, mileage, purchase_price, serial_number)
+        self._doors = doors
+        self._wheels = wheels
 
         
 class Motorcycle(Vehicle):
 
-    ######## CODE MISSING HERE
+    classic_count = 0
 
     def __init__(self,year,mileage,purchase_price,serial_number,classic=False):
-        pass
-    ######## CODE MISSING HERE
+        super().__init__(year, mileage, purchase_price, serial_number)
+        self._classic = classic
+        if classic:
+            Motorcycle.classic_count += 1
     
 
 ### test cases ###
@@ -56,7 +61,7 @@ Veh4 = Lorry(2005,180000,16000,"hbh997123f",6)
 Veh5 = Lorry(2013,30000,35000,"hjbf17jbkh",8,4)
 Veh6 = Motorcycle(1975,75500,40000,"bh545664rh",True)
 
-#print(Veh1,Veh2,Veh3,Veh4,Veh5,Veh6)
+print(Veh1,Veh2,Veh3,Veh4,Veh5,Veh6)
 #prints 	100000 	100001 	100002 	100003 	100004 	100005
 
 # Veh7 = Motorcycle("year",10000,25000,"bjhgss4rdh",False)
